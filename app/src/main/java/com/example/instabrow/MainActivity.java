6f3854747d4abf93db140248309f314711f1267a
giabr <31702129+giabr@users.noreply.github.com>
2019-03-14 23:05:39 +0700

    Update sign up

--- app/src/main/java/com/example/instabrow/MainActivity.java
+++ app/src/main/java/com/example/instabrow/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.instabrow;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    private FirebaseAuth mAuth;
+    EditText emailView;
+    EditText passwordView;
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailView = findViewById(R.id.email);
+        passwordView = findViewById(R.id.password);
+        signUp = findViewById(R.id.signUp);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp(emailView.getText().toString(),passwordView.getText().toString());
+            }
+        });
+
+    }
+
+    //Sign up
+    public void signUp(String email, String pass){
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)){
+            Toast.makeText(this, "Username and Password required", Toast.LENGTH_SHORT).show();
+        }
+        else if (pass.length() < 6){
+            Toast.makeText(this, "Password should be at least 6 characters", Toast.LENGTH_SHORT).show();
+        }else {
+            mAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.i("Status", "createUserWithEmail:success");
+                                Toast.makeText(getApplicationContext(), "Account succesfully created", Toast.LENGTH_SHORT).show();
+//                                FirebaseUser user = mAuth.getCurrentUser();
+                            }else {
+                                // If sign in fails, display a message to the user.
+                                Log.i("Status", "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+        }
     }
 }
